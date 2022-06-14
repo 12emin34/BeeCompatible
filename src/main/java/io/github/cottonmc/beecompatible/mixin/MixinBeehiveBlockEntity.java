add77724b6b1f8bbcd6e889c7cec9b9d4f6ead25
12emin34 <macanovic.emin@gmail.com>
2022-06-14 17:23:47 +0200

    Port to 1.16.2

--- src/main/java/io/github/cottonmc/beecompatible/mixin/MixinBeehiveBlockEntity.java
+++ src/main/java/io/github/cottonmc/beecompatible/mixin/MixinBeehiveBlockEntity.java
@@ -5,39 +5,49 @@ import io.github.cottonmc.beecompatible.api.BeeWeatherCheckCallback;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BeehiveBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.World;
+import net.minecraft.util.Tickable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
 @Mixin(BeehiveBlockEntity.class)
-public class MixinBeehiveBlockEntity {
+public abstract class MixinBeehiveBlockEntity extends BlockEntity implements Tickable {
 
-	@Redirect(method = "releaseBee", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isNight()Z"))
-	private boolean timeEvent(World world, BlockState state, CompoundTag tag, List<Entity> entities, BeehiveBlockEntity.BeeState beeState) {
-		Entity entity = EntityType.loadEntityWithPassengers(tag, world, e -> e);
-		if (entity instanceof BeeEntity) {
-			BeeEntity bee = (BeeEntity)entity;
-			TriState result = BeeTimeCheckCallback.EVENT.invoker().checkTime(world, bee);
-			if (result != TriState.DEFAULT) return !result.get(); //a negative here allows bees to exit
-		}
-		return world.isNight();
-	}
+    public MixinBeehiveBlockEntity(BlockEntityType<?> type) {
+        super(type);
+    }
 
-	@Redirect(method = "releaseBee", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isRaining()Z"))
-	private boolean weatherEvent(World world, BlockState state, CompoundTag tag, List<Entity> entities, BeehiveBlockEntity.BeeState beeState) {
-		Entity entity = EntityType.loadEntityWithPassengers(tag, world, e -> e);
-		if (entity instanceof BeeEntity) {
-			BeeEntity bee = (BeeEntity)entity;
-			TriState result = BeeWeatherCheckCallback.EVENT.invoker().checkWeather(world, bee);
-			if (result != TriState.DEFAULT) return !result.get(); //a negative here allows bees to exit
-		}
-		return world.isRaining();
-	}
+    @Inject(method = "releaseBee", at = @At(value = "TAIL"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void timeEvent(BlockState state, BeehiveBlockEntity.Bee bee, List<Entity> list, BeehiveBlockEntity.BeeState beeState, CallbackInfoReturnable<Boolean> cir) {
+        CompoundTag compoundTag = bee.entityData;
+        Entity entity = EntityType.loadEntityWithPassengers(compoundTag, world, e -> e);
+        if (entity instanceof BeeEntity) {
+            BeeEntity beeEntity = (BeeEntity) entity;
+            TriState result = BeeTimeCheckCallback.EVENT.invoker().checkTime(world, beeEntity);
+            if (result != TriState.DEFAULT) cir.setReturnValue(!result.get()); //a negative here allows bees to exit
+        }
+        cir.setReturnValue(world.isNight());
+    }
+
+    @Inject(method = "releaseBee", at = @At(value = "TAIL"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void weatherEvent(BlockState state, BeehiveBlockEntity.Bee bee, List<Entity> list, BeehiveBlockEntity.BeeState beeState, CallbackInfoReturnable<Boolean> cir) {
+        CompoundTag compoundTag = bee.entityData;
+        Entity entity = EntityType.loadEntityWithPassengers(compoundTag, world, e -> e);
+        if (entity instanceof BeeEntity) {
+            BeeEntity beeEntity = (BeeEntity) entity;
+            TriState result = BeeWeatherCheckCallback.EVENT.invoker().checkWeather(world, beeEntity);
+            if (result != TriState.DEFAULT) cir.setReturnValue(!result.get()); //a negative here allows bees to exit
+        }
+        cir.setReturnValue(world.isRaining());
+    }
 }
